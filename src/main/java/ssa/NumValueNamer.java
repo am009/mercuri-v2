5ac8690ad16eeac206cf1d993691c77160c1de6b
wjk <634960348@qq.com>
2022-08-12 22:05:29 +0800

    Fix NumValueNamer: preserve name.

--- src/main/java/ssa/NumValueNamer.java
+++ src/main/java/ssa/NumValueNamer.java
@@ -12,7 +12,8 @@ import ssa.ds.Type;
 import ssa.ds.Value;
 
 /**
- * NumValueNamer 包含一个计数器状态变量，用于给所有的 Value 提供一个唯一的临时名称 
+ * NumValueNamer 包含一个计数器状态变量，用于给所有的 Value 提供一个唯一的临时数字名称。
+ * 纯数字名称仅用于临时名称，因此其他部分命名时需要避开纯数字的名字。 
  * 
  * basicBlock的label还是生成的时候去命名，在ctx里放个index防止重名
  * 全局变量和函数参数也应当在生成时命名。语法里好像函数参数的名字不能省略。
@@ -44,10 +45,15 @@ public class NumValueNamer {
         i.getUses().forEach(use -> visitValue(use.value));
     }
 
+    public boolean isTempName(String name) {
+        return name == null || name.matches("[0-9]+");
+    }
+
     public Map<Value, Boolean> visited = new HashMap<>();
 
     public void visitValue(Value i) {
-        if ((!i.type.equals(Type.Void)) && (!(i instanceof ConstantValue))) {
+        if ((!i.type.equals(Type.Void)) && isTempName(i.name) && (!(i instanceof ConstantValue))) {
+            // 当后面的指令引用前面指令的返回值的时候，会再次访问前面的指令。
             if (!visited.containsKey(i)) {
                 i.name = String.valueOf(count++);
                 visited.put(i, true);
